720cab5e8761a68d957f9822c76d862ca93a7920
poisonpanicyvy <poisonpanicyvy@web.de>
2023-01-19 11:51:46 +0100

    1. array

--- script_01.js
+++ script_01.js
@@ -7,16 +7,26 @@
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-output(getSentence("Ich", "bin", "Poison","Panic","Yvy"));
-function getSentence(word1,word2,word3,word4,word5) {
-const GAP = " ";
-const PUNCT = ".";
-    let str = word1 + GAP + word2 + GAP + word3 + GAP + word4 +GAP+ word5 + PUNCT;
-    return str;
-}
+// output(getSentence("Ich", "bin", "Poison","Panic","Yvy"));
+// function getSentence(word1,word2,word3,word4,word5) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//         let str = word1 + GAP + word2 + GAP + word3 + GAP + word4 +GAP+ word5 + PUNCT;
+//         return str;
+//     }
+    
+
+let arr;
+arr =[];
+arr = [2,7,11,4];
+arr = [true,false,true];
+arr = ["Ich","bin","Yvonne","Schubert"];
 
 
-
+output(arr);
+output(arr.length);
+output(arr[0]);
+output(arr[2]);
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
