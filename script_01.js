33f116397b976050b779940835bc3a7d87b4242f
poisonpanicyvy <poisonpanicyvy@web.de>
2023-01-19 10:58:29 +0100

    Array beginn

--- script_01.js
+++ script_01.js
@@ -7,7 +7,13 @@
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-
+output(getSentence("Ich", "bin", "Poison","Panic","Yvy"));
+function getSentence(word1,word2,word3,word4,word5) {
+const GAP = " ";
+const PUNCT = ".";
+    let str = word1 + GAP + word2 + GAP + word3 + GAP + word4 +GAP+ word5 + PUNCT;
+    return str;
+}
 
 
 
